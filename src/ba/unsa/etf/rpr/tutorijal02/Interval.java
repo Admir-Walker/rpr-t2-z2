472e959ac2c19cad7ac0ca0f44a1a18e1aa45211
Admir-Walker <aihtijarev2@etf.unsa.ba>
2020-11-02 12:27:19 +0100

    Implementirana tijela oba konstruktora.

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -2,13 +2,22 @@ package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
 
-    public Interval(double v, double v1, boolean b, boolean b1) {
+    private double pocetnaTacka, krajnjaTacka;
+    boolean pripadnostIntervaluPocetneTacke, pripadnostIntervaluKrajnjeTacke;
 
-
+    public Interval(double pocetnaTacka, double krajnjaTacka, boolean pripadnostIntervaluPocetneTacke, boolean pripadnostIntervaluKrajnjeTacke) throws IllegalArgumentException {
+        if(pocetnaTacka > krajnjaTacka) throw new IllegalArgumentException();
+        this.pocetnaTacka = pocetnaTacka;
+        this.krajnjaTacka = krajnjaTacka;
+        this.pripadnostIntervaluPocetneTacke = pripadnostIntervaluPocetneTacke;
+        this.pripadnostIntervaluKrajnjeTacke = pripadnostIntervaluKrajnjeTacke;
     }
 
     public Interval() {
-
+        pocetnaTacka = 0.0;
+        krajnjaTacka = 0.0;
+        pripadnostIntervaluPocetneTacke = false;
+        pripadnostIntervaluKrajnjeTacke = false;
     }
 
     public static Interval intersect(Interval i, Interval i2) {
